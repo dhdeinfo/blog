0687dee03cb02dbe8e63778a297b673c30d860dc
Adiyan Mustahim <50477931+dhdeinfo@users.noreply.github.com>
2022-10-31 09:55:01 +0700

    Delete indo.js

--- indo.js
+++
@@ -1,8 +0,0 @@
-function autoRelated(data) {
-    var out = data[1];		    
-
-    for (i = 0; i < out.length; i++) {
-    	var kw = out[i];
-        dom += `<aside><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" /><small>title: <i>${kw}</i></small><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3><p align="justify"><b>${kw}</b> adalah salah satu hasil terbaik untuk ${postTitle}. Segala sesuatu di sini adalah untuk referensi perpose saja. Jangan ragu untuk menyimpan dan mem-bookmark ${kw} pada postingan ini.</p> </aside>`;
-    }
-}
